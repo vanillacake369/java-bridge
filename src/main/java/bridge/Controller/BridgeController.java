6b8a37a190833cc373749ace0337c66465b4abdc
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-11-18 20:59:23 +0900

    docs: - 유의사항으로서 프로그래밍 요구사항 추가 // README.md

--- src/main/java/bridge/Controller/BridgeController.java
+++ src/main/java/bridge/Controller/BridgeController.java
@@ -1,5 +1,17 @@
 package bridge.Controller;
 
 public class BridgeController {
+    private static BridgeController bridgeController = new BridgeController();
+    private BridgeController(){};
+    public static BridgeController getInstance(){
+        return bridgeController;
+    }
+
+    public void run(){
+        // 초기 세팅 입력
+        // 입력된 세팅에 대해 다리 생성
+        // 게임 시작
+        //
+    }
 
 }
