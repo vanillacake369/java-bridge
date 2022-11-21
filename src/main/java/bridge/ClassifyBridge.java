6388dd253c3d95b1c5dd625bb877a4479cbd12b5
NGN-Limjihoon <lonelynight1026@gmail.com>
2022-11-21 19:48:08 +0900

    feat: - BridgeMaker : class   - 입력받은 기이에 따라 다리 형성 - ClassifyBridge : enum   - 위 칸이면 U, 아래 칸이면 D 판별기 생성

---
+++ src/main/java/bridge/ClassifyBridge.java
@@ -0,0 +1,10 @@
+package bridge;
+
+public class ClassifyBridge {
+    public static String classify(int step){
+        if(step == 0){
+            return "D";
+        }
+        return "U";
+    }
+}
